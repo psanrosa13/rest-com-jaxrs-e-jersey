90933dea79755b8afad461499724a1ed738dfc38
psanrosa13 <psanrosa13@gmail.com>
2017-07-22 18:29:04 -0300

    servidor local e ajustes do carrinho resource e testes

--- src/main/java/br/com/alura/loja/Servidor.java
+++ src/main/java/br/com/alura/loja/Servidor.java
@@ -1,5 +1,6 @@
 package br.com.alura.loja;
 
+import java.io.IOException;
 import java.net.URI;
 
 import org.glassfish.grizzly.http.server.HttpServer;
@@ -9,14 +10,18 @@ import org.glassfish.jersey.server.ResourceConfig;
 public class Servidor {
 
 	
-	public static void main(String[] args) {
-		URI uri = URI.create("http://localhost:8080/");
-		ResourceConfig config = new ResourceConfig().packages("br.com.alura.loja");
-		HttpServer server = GrizzlyHttpServerFactory.createHttpServer(uri, config);
+	public static void main(String[] args) throws IOException {
+		HttpServer server = inicializaServidor();
 		System.out.println("Servidor rodando");
 		System.in.read();
 		server.stop();
 		
-	
+	}
+
+	public static HttpServer inicializaServidor() {
+		URI uri = URI.create("http://localhost:8080/");
+		ResourceConfig config = new ResourceConfig().packages("br.com.alura.loja");
+		HttpServer server = GrizzlyHttpServerFactory.createHttpServer(uri, config);
+		return server;
 	}
 }
--- src/test/java/ClienteTeste.java
+++ src/test/java/ClienteTeste.java
@@ -1,20 +1,55 @@
+import java.net.URI;
+
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.WebTarget;
 
 import junit.framework.Assert;
 
+import org.glassfish.grizzly.http.server.HttpServer;
+import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
+import org.glassfish.jersey.server.ResourceConfig;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
+
+import br.com.alura.loja.Servidor;
+import br.com.alura.loja.modelo.Carrinho;
+
+import com.thoughtworks.xstream.XStream;
 
 
 public class ClienteTeste {
 
+	private HttpServer server;
+	
+	@Before
+    public void before() {
+       this.server = Servidor.inicializaServidor();
+    }
+
+    @After
+    public void mataServidor() {
+        server.stop();
+    }
+	
+	
 	@Test
 	public void testaQueAConexaoComOServidorFunciona(){
 		Client client = ClientBuilder.newClient();
 		WebTarget target = client.target("http://www.mocky.io");
 		String conteudo= target.path("/v2/52aaf5deee7ba8c70329fb7d").request().get(String.class);
 		Assert.assertTrue(conteudo.contains("<rua>Rua Vergueiro 3185"));
+	}
+	
+	@Test
+	public void testaQueBuscarUmCarrinhoTrazOCarrinhoEsperado() {
+		Client client = ClientBuilder.newClient();
+		WebTarget target = client.target("http://localhost:8080");
+		String conteudo= target.path("/carrinhos/1").request().get(String.class);
+		Carrinho carrinho = (Carrinho) new XStream().fromXML(conteudo);
+		Assert.assertEquals("Rua Vergueiro 3185, 8 andar", carrinho.getRua());
+	
 	}
 
 
